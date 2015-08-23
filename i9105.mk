23be3f558fdac26f1e6eb70d90a2ec6b808b4468
ghsr <ghsr92@yandex.ru>
2015-08-23 21:01:36 +0600

    i9105: Switch to AICP

--- i9105.mk
+++ i9105.mk
@@ -1,9 +1,19 @@
-# Include common makefile
-$(call inherit-product, device/samsung/galaxys2plus-common/common.mk)
+# Specify phone tech before including common
+$(call inherit-product, vendor/aicp/configs/gsm.mk)
 
-# Init scripts
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/ramdisk/init.capri_ss_s2ve.rc:root/init.capri_ss_s2ve.rc \
-    $(LOCAL_PATH)/ramdisk/init.recovery.capri_ss_s2ve.rc:root/init.recovery.capri_ss_s2ve.rc \
-    $(LOCAL_PATH)/ramdisk/ueventd.capri_ss_s2ve.rc:root/ueventd.capri_ss_s2ve.rc \
-    $(LOCAL_PATH)/ramdisk/fstab.capri_ss_s2ve:root/fstab.capri_ss_s2ve
+# Inherit some common AICP stuff
+$(call inherit-product, vendor/aicp/configs/common.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/i9105/full_i9105.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := i9105
+PRODUCT_NAME := aicp_i9105
+
+# Set build fingerprint / ID / Prduct Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=s2vexx \
+    TARGET_DEVICE=s2ve \
+    BUILD_FINGERPRINT="samsung/s2vexx/s2ve:4.2.2/JDQ39/I9105XXUBND1:user/release-keys" \
+    PRIVATE_BUILD_DESC="s2vexx-user 4.2.2 JDQ39 I9105XXUBND1 release-keys"
